e784efd4ca8c1cf0ce71c5ba0b797d182791ef97
FraidOfWater <141600613+FraidOfWater@users.noreply.github.com>
2025-04-10 13:21:20 +0300

    Update gen_shortcut.py

--- gen_shortcut.py
+++ gen_shortcut.py
@@ -1,10 +1,10 @@
 import subprocess
 import os
 import argparse
+import shutil
 
 
-
-def create_shortcut(arguments, verbose=True, silent=False):
+def create_shortcut(arguments, force=False, verbose=True, silent=False):
     """
     Creates shortcuts from a list of tuples ('filepath', shortcutpath').
     
@@ -28,14 +28,16 @@ def create_shortcut(arguments, verbose=True, silent=False):
                     filepath_directory = os.path.dirname(filepath)
                     shortcut_path = argument[1]
                     shortcut_directory = os.path.dirname(shortcut_path)
-                    
+                    save = shortcut_directory
+                    if force:
+                        shortcut_directory = os.getcwd()
                     arguments = f'"{filepath}" "{filepath_directory}" "{shortcut_directory}" "{os.path.basename(shortcut_path)}"'
 
-                    os.makedirs(os.path.dirname(os.path.join(shortcut_directory, os.path.basename(filepath))), exist_ok=True)
+                    os.makedirs(os.path.dirname(shortcut_path), exist_ok=True)
 
-                    if not os.path.exists(f"{os.path.join(shortcut_directory, os.path.basename(filepath))}.lnk"):
+                    if not os.path.exists(f"{shortcut_path}.lnk"):
                         args.write(arguments + "\n")
-                        validate_these.append(f"{os.path.join(shortcut_directory, os.path.basename(filepath))}.lnk")
+                        validate_these.append((f"{os.path.join(shortcut_directory, os.path.basename(filepath))}.lnk", save, filepath))
         except Exception as e:
             print("Error generating arguments.txt", e)
     def launch_subprocess():
@@ -59,12 +61,12 @@ def create_shortcut(arguments, verbose=True, silent=False):
                     print(line)
             if not silent:
                 errors = 0
-                for line in process.stdout:
+                for line in process.stderr:
+                    print(line)
                     errors += 1
                 if errors != 0:
                     print(f"ERRORS: {errors}")
-                for line in process.stdout:
-                    print(line)
+                    
 
         except subprocess.CalledProcessError as e:
             print(f"Error: {e}")
@@ -73,15 +75,41 @@ def create_shortcut(arguments, verbose=True, silent=False):
     def validate():
         validated = 0
         errors = 0
+        retry = []
         for x in validate_these:
-            if os.path.exists(x) and os.path.isfile(x):
-                validated += 1
-                pass
+            if os.path.exists(x[0]) and os.path.isfile(x[0]):
+                if force:
+                    try:
+                        new_path = os.path.join(x[1], os.path.basename(x[0]))
+                        shutil.move(x[0], x[1])
+
+                        if os.path.exists(new_path) and os.path.isfile(new_path):
+                            validated += 1
+                    except Exception as e:
+                        if not silent:
+                            print("Error:", e)
+                        errors += 1
+                    pass
             else:
-                print("Shortcut wasn't created:", x)
+                p = 0
+                if not silent:
+                    print("Shortcut wasn't created:", x)
+                    retry.append(x)
                 errors += 1
-        print(f"Shortcuts created: {validated} with {errors} errors.")
-            
+
+        if not silent:
+            print(f"Shortcuts created: {validated} with {errors} errors.")
+        if retry:
+            for x in retry:
+                arguments = [
+                    r"CreateShortcut.exe",
+                    os.path.realpath(x[2]),
+                    os.path.dirname(os.path.realpath(x[2])),
+                    os.path.realpath(os.getcwd()), 
+                    os.path.basename(x[2])
+                ]
+                
+                create_shortcut_command_line(arguments, x[1])
     validate_these = []
     if os.path.isfile("arguments.txt"):
         os.remove("arguments.txt")
@@ -90,18 +118,22 @@ def create_shortcut(arguments, verbose=True, silent=False):
     if not silent:
         validate()
 
-def create_shortcut_command_line(arguments):
+def create_shortcut_command_line(arguments, save):
     # Run the executable with arguments
     try:
         result = subprocess.run(arguments, capture_output=True, text=True, check=True)
         #print("Program output:", result.stdout)  # Output of the program
         if result.stderr != '':
             print("Program error:", result.stderr)   # Error output (if any)
+        
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}")
     except FileNotFoundError:
         print("The executable file was not found.")
-
+    try:
+        shutil.move(f"{os.path.join(arguments[3], arguments[4])}.lnk", save)
+    except Exception as e:
+        print(e)
 def main():
     # Set up argument parser
     parser = argparse.ArgumentParser(description='CreateShortcut.exe and this script must be located in the same directory. This script creates shortcuts.')
@@ -113,12 +145,11 @@ def main():
 
     # Parse the arguments
     args = parser.parse_args()
-
     # If no shortcut_directory is provided, use the Desktop
-    if not args.shortcut_directory:
-        # Get the Desktop path
-        desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
-        args.shortcut_directory = desktop_path
+    save = args.shortcut_directory
+    # Get the Desktop path
+    desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+    args.shortcut_directory = desktop_path
 
     # If no shortcut_name is provided, use the filename of the target_path
     if not args.shortcut_name:
@@ -134,7 +165,7 @@ def main():
     ]
 
     # Call the function to create the shortcut
-    create_shortcut_command_line(arguments)
+    create_shortcut_command_line(arguments, save)
 
 if __name__ == "__main__":
     main()
